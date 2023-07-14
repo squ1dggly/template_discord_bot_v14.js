15c9b84d31b6ae68c91fa4dddf6f94e353c634f7
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-07-13 19:45:39 -1000

    init

---
+++ modules/jsTools/array.js
@@ -0,0 +1,30 @@
+const _o = require("./object");
+
+/** Split an array into smaller arrays that don't exceed a given size
+ * @param {array} arr array to split
+ * @param {number} size max size before splitting
+ * @param {boolean} copy return a deep copy of the array using structuredClone() */
+function chunk(arr, size, copy = false) {
+	if (!Array.isArray(arr)) throw new TypeError("A valid array must be provided");
+	if (size <= 0) throw new Error("Size cannot be 0 or negative");
+	if (!arr.length || arr.length < size) return arr;
+
+	let chunk = [];
+
+	// Iterate through the array
+	for (let i = 0; i < arr.length; i += size) {
+		// Slice the array from the current index
+		chunk = arr.slice(i, i + size);
+	}
+
+	return deepCopy ? structuredClone(chunk) : chunk;
+}
+
+/** Filter out non-unique items from an array
+ * @param {Array} arr array to filter
+ * @param {string} prop a nested property within each item to filter by */
+function unique(arr, prop = "") {
+	return [...new Map(arr.map(item => [_o.getProp(item, prop)], item)).values()];
+}
+
+module.exports = { chunk, unique };
---
+++ modules/jsTools/object.js
@@ -0,0 +1,54 @@
+/** Get a property value from an object using the provided path
+ * @param {{}} obj object
+ * @param {string} path path to the nested property
+ *
+ * @example // returns 5
+ * let obj = { a: 5 };
+ * getProp(obj, "a")
+ * 
+ * @example // returns "hello, world!"
+ * let obj = { a: [{ content: "hello, world!" }] };
+ * getProp(obj, "a[0].content") */
+function getProp(obj, path) {
+	if (typeof obj !== "object") throw new TypeError("You must provide a valid object");
+	if (typeof path !== "string") throw new TypeError("You must provide a valid path string");
+	if (!path.trim()) return obj;
+
+	path = path
+		// Strip whitespace
+		.trim()
+		// Replace array indexes with property index values
+		.replace(/\[(\w+)\]/g, ".$1")
+		// Strip leading dots
+		.replace(/^\./, "");
+
+	// Split path into an array of property names
+	let _path = path.split(".");
+
+	// Used for debugging where we were at before throwing an error
+	let debug_path = [];
+
+	// Iterate through each property path strings
+	for (let i = 0; i < _path.length; ++i) {
+		// Get the current property path we're at
+		let prop = _path[i];
+
+		// DEBUGGING
+		debug_path.push(prop);
+
+		try {
+			// Check if the property exists
+			if (prop in obj && obj[prop] === undefined)
+				throw new Error(`Object property \'${debug_path.join(".")}\' is undefined`);
+
+			// Set obj to the new value
+			obj = obj[prop];
+		} catch {
+			throw new Error(`Cannot get property \'${prop}\' from \'${obj}\'`);
+		}
+	}
+
+	return obj;
+}
+
+module.exports = { getProp };
