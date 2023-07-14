239ea7e9e57be11d9c0292c195e8741015d2baa8
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-07-13 19:54:42 -1000

    init

---
+++ modules/jsTools/string.js
@@ -0,0 +1,12 @@
+/** Converts the first letter of every alphanumeric word to uppercase
+ * @param {string} str string to convert */
+function toTitleCase(str) {
+	return str.replace(
+		/\w\S*/g,
+		txt => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase()
+	);
+}
+
+function formatNumber(num, options) {}
+
+function appendNumberPlace(num) {}
