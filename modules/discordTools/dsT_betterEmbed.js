158fb696567691e1ee1e11319bb5f7c656413839
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-07-22 15:02:42 -1000

    init

--- modules/discordTools/dsT_betterEmbed.js
+++ modules/discordTools/dsT_betterEmbed.js
@@ -37,14 +37,14 @@ const config = require("./_dsT_config.json");
 const { Message, CommandInteraction, User, GuildMember, ActionRowBuilder, EmbedBuilder } = require("discord.js");
 const _jsT = require("../jsTools/_jsT");
 
-class BetterEmbed {
-	#embed = new EmbedBuilder();
-
+class BetterEmbed extends EmbedBuilder {
 	/** Send a confirmation message and await the user's response
 	 * @param {bE_options} options */
 	constructor(options) {
+		super();
+
 		// prettier-ignore
-		this.data = {
+		this.options = {
 			interaction: null,
 			author: { user: null, text: "", iconURL: "", linkURL: "" },
             title: { text: "", linkURL: "" }, footer: { text: "", linkURL: "" },
@@ -52,31 +52,58 @@ class BetterEmbed {
             color: config.EMBED_COLOR || null,
             showTimestamp: false, ...options
 		};
+
+		this.#configure();
 	}
 
 	#configure() {
-		let _embed = this.#embed;
-
 		/// Apply shorthand formatting
-		this.data.description = this.#format(this.data.description);
-		this.data.author.text = this.#format(this.data.author.text);
-		this.data.title.text = this.#format(this.data.title.text);
-		this.data.footer.text = this.#format(this.data.footer.text);
+		this.data.description = this.#format(this.options.description);
+		this.data.author.name = this.#format(this.options.author.text);
+		this.data.title = this.#format(this.options.title.text);
+		this.data.footer.text = this.#format(this.options.footer.text);
 
-		// Author
-		// prettier-ignore
-		if (this.data.author.text) _embed.setAuthor({
-			name: this.data.author.text,
-			url: _embed.data.author.icon_url
-		});
+		/// Author
+		if (this.data.author.text) this.data.author.name = this.options.author.text;
+		if (this.data.author.linkURL) this.data.author.url = this.options.author.linkURL;
+		/* if ((this.options.author.user || this.options.author.iconURL) && this.options.author.iconURL !== (false || null)) {
+			// Get the authorURL() method from within the GuildMember or from the user itself
+			let _userAvatarURL = this.options.author?.user?.user?.avatarURL || this.options.author?.user?.avatarURL;
+			this.data.author.icon_url = _userAvatarURL({ dynamic: true });
+		} */
+
+		if ((this.options.author.user || this.options.author.iconURL) && this.options.author.iconURL !== (false || null)) {
+			// Get the authorURL() method from within the GuildMember or from the user itself
+			let _foo_avatarURL = this.options.author?.user?.user?.avatarURL || this.options.author?.user?.avatarURL;
+			let _avatarURL = _foo_avatarURL({ dynamic: true });
+
+			// prettier-ignore
+			try { this.setAuthor({ iconURL: _avatarURL }); }
+			catch { logger.error("Could not configure embed", `invalid title URL: \`${_avatarURL}\``); }
+		}
 
 		// Title
-		if (this.data.title.text) _embed.setTitle(this.data.title.text);
+		if (this.options.title.text) this.setTitle(this.options.title.text);
 		// Title URL
-		if (this.data.title.linkURL) _embed.setURL(this.data.title.linkURL);
+		// prettier-ignore
+		if (this.options.title.linkURL)
+			try { this.setURL(this.options.title.linkURL); }
+			catch { logger.error("Could not configure embed", `invalid title URL: \`${this.options.title.linkURL}\``); }
 
-		// Image
-		if (this.data.imageURL) _embed.setImage(this.data.imageURL);
+		// Image URL
+		// prettier-ignore
+		if (this.options.imageURL)
+			try { this.setImage(this.options.imageURL); }
+			catch { logger.error("Could not configure embed", `invalid image URL: \`${this.options.imageURL}\``); }
+
+		// Image thumbnail URL
+		// prettier-ignore
+		if (this.options.thumbnailURL)
+			try { this.setThumbnail(this.options.thumbnailURL); }
+			catch { logger.error("Could not configure embed", `invalid thumbnail URL: \`${this.options.thumbnailURL}\``); }
+
+		// Description
+		if (this.options.description) this.data.description = this.options.description;
 	}
 
 	#format(str) {
@@ -87,4 +114,5 @@ class BetterEmbed {
 }
 
 new BetterEmbed();
-embed.options.embed.module.exports = BetterEmbed;
+
+module.exports = BetterEmbed;
