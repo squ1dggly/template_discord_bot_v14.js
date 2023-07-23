68b4d3b073b47268624bce6a2a7be8af768c4e09
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-07-23 03:00:50 -1000

    init

---
+++ modules/discordTools/dsT_embedNavigator.js
@@ -0,0 +1,10 @@
+const config = require("./_dsT_config.json");
+
+const { CommandInteraction, TextChannel, ActionRowBuilder, EmbedBuilder } = require("discord.js");
+const _jsT = require("../jsTools/_jsT");
+
+class EmbedNavigator {
+	constructor(eN_options) {}
+}
+
+module.exports = EmbedNavigator;
