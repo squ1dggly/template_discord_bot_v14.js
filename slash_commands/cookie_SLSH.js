5e7f456db1b9ca5a7f07d98028540bbebfb91d66
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-07-30 02:24:59 -1000

    init

--- slash_commands/cookie_SLSH.js
+++ slash_commands/cookie_SLSH.js
@@ -1,25 +1,27 @@
-const { Client, CommandInteraction, SlashCommandBuilder } = require('discord.js');
+const { Client, CommandInteraction, SlashCommandBuilder } = require("discord.js");
 
-const { randomTools } = require('../modules/jsTools');
+const _jsT = require("../modules/jsTools/_jsT");
 
 module.exports = {
-    builder: new SlashCommandBuilder().setName("cookie")
+	// prettier-ignore
+	builder: new SlashCommandBuilder().setName("cookie")
         .setDescription("Get a cookie, or a glass of milk."),
 
-    // ownerOnly: true, // Only allow the owner and admins defined in config_client to use this command
-    // requireGuildAdmin: true, // Only allow users with admin to use this command
+	// ownerOnly: true, // Only allow the owner and admins defined in config_client to use this command
+	// requireGuildAdmin: true, // Only allow users with admin to use this command
 
-    /**
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     */
-    execute: async (client, interaction) => {
-        let choices = [
+	/**
+	 * @param {Client} client
+	 * @param {CommandInteraction} interaction
+	 */
+	execute: async (client, interaction) => {
+		// prettier-ignore
+		let choices = [
             "Hey, %USER! Have a cookie! :cookie:",
             "%USER. It's your lucky day! Have a glass of milk! :milk:",
         ];
 
-        let response = randomTools.choice(choices).replace("%USER", interaction.user);
-        return await interaction.reply({ content: response });
-    }
+		let response = _jsT.choice(choices).replace("%USER", interaction.user);
+		return await interaction.reply({ content: response });
+	}
 };
