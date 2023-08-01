c5e2c8b939f4fd4ec73a0d3bbed3d724042a4927
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-08-01 03:41:37 -1000

    init

--- slash_commands/cookie_SLSH.js
+++ slash_commands/cookie_SLSH.js
@@ -1,7 +1,7 @@
 const { Client, CommandInteraction, SlashCommandBuilder } = require("discord.js");
 
-const { BetterEmbed } = require(`../modules/discordTools/_dsT`);
-const _jsT = require(`../modules/jsTools/_jsT`);
+const { BetterEmbed } = require("../modules/discordTools/_dsT");
+const _jsT = require("../modules/jsTools/_jsT");
 
 module.exports = {
 	options: { icon: "🍪", deferReply: false },
