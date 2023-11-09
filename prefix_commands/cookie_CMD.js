0c121fcbd8996f0dbbae6c727eab5762ca245649
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-11-09 01:04:14 -1000

    updates

--- prefix_commands/cookie_CMD.js
+++ prefix_commands/cookie_CMD.js
@@ -4,23 +4,23 @@ const { BetterEmbed } = require("../modules/discordTools");
 const _jsT = require("../modules/jsTools");
 
 module.exports = {
-	name: "cookie",
-	options: { icon: "🍪", deferReply: false, botAdminOnly: false, guildAdminOnly: false },
+    name: "cookie",
+    options: { icon: "🍪", deferReply: false, botAdminOnly: false, guildAdminOnly: false },
 
-	/** @param {Client} client @param {Message} message */
-	execute: async (client, message) => {
-		// prettier-ignore
-		let choices = [
+    /** @param {Client} client @param {Message} message */
+    execute: async (client, message) => {
+        // prettier-ignore
+        let choices = [
             "What's up, **$USERNAME**! Have a cookie! :cookie:",
             "Hey, **$USERNAME**! Have a glass of milk! :milk:",
         ];
 
-		// prettier-ignore
-		let embed_cookie = new BetterEmbed({
-            channel: message.channel, author: { iconURL: true },
+        // prettier-ignore
+        let embed_cookie = new BetterEmbed({
+            channel: message.channel, author: { user: message.author, iconURL: true },
             description: _jsT.choice(choices), timestamp: true
         });
 
-		return await embed_cookie.send();
-	}
+        return await embed_cookie.send();
+    }
 };
