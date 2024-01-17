14aa7e59625bcd8269242dc1aa1d49fea54d85ee
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-01-17 01:42:24 -1000

    bug fixes

--- events/interaction/create/9_processSlashCommand.js
+++ events/interaction/create/9_processSlashCommand.js
@@ -1,6 +1,6 @@
 /** @file Execute commands requested by a command interaction @author xsqu1znt */
 
-const { Client, PermissionFlagBits, GuildMember, BaseInteraction } = require("discord.js");
+const { Client, PermissionFlagsBits, GuildMember, BaseInteraction } = require("discord.js");
 const { BetterEmbed } = require("../../../utils/discordTools");
 const logger = require("../../../utils/logger");
 
@@ -17,13 +17,13 @@ function userIsBotAdminOrBypass(interaction) {
 
 /** @param {BaseInteraction} interaction */
 function userIsGuildAdminOrBypass(interaction) {
-	let isAdmin = interaction.member.permissions.has(PermissionFlagBits.Flags.Administrator);
+	let isAdmin = interaction.member.permissions.has(PermissionFlagsBits.Flags.Administrator);
 	let canBypass = userIsBotAdminOrBypass(interaction);
 
 	return isAdmin || canBypass;
 }
 
-/** @param {GuildMember} guildMember @param {PermissionFlagBits[]} permissions */
+/** @param {GuildMember} guildMember @param {PermissionFlagsBits[]} permissions */
 function hasSpecialPermissions(guildMember, permissions) {
 	let has = [];
 	let missing = [];
