cd681efb7e62dc768bf9d83cdc90f6f1af93a888
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-02-18 21:35:34 -1000

    syntax

--- utils/discordTools/dT_deleteMessageAfter.js
+++ utils/discordTools/dT_deleteMessageAfter.js
@@ -4,7 +4,7 @@ const jt = require("../jsTools");
 const config = require("./dT_config.json");
 
 /** Delete a message after a given amount of time
- * @param {Message} message message object that was sent
+ * @param {Message|Promise<Message>} message message object that was sent
  * @param {number|string} time amount of time to wait in milliseconds */
 async function deleteMesssageAfter(message, time = config.timeouts.ERROR_MESSAGE) {
 	time = jt.parseTime(time);
