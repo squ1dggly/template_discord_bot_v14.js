eb4eecbe427b1fe8f00fbc3578336b10d951d608
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-03-06 04:42:32 -1000

    update

--- utils/jsTools/jT_date.js
+++ utils/jsTools/jT_date.js
@@ -1,47 +1,64 @@
 const _nT = require("./jT_number");
 
-// prettier-ignore
 /** @typedef parse_options
  * @property {"ms"|"s"} type return "s" (seconds) or "ms" (milliseconds)
  * @property {boolean} fromNow add Date.now() to the result */
 
 /** Parse a string into either milliseconds or seconds
- * @param {string} str string to parse
+ * @param {string|number} str string to parse
  * @param {parse_options} options
  *
  * @example
- * parse("1m", "s") --> 60
- * parse("-1m", "s") --> -60 */
+ * parse("1m") --> 60000
+ * parse("1h 30m") --> 5400000
+ *
+ * parse("1h", { fromNow: true }) --> Date.now() + 3600000
+ * parse("-1m", { type: "s" }) --> -60 */
 function parseTime(str, options) {
 	options = { type: "ms", fromNow: false, ...options };
-	if (!isNaN(+str)) return +str;
-	
-    let isNegative = str.at(0) === "-";
-	let time = str.match(/[0-9]+|[a-zA-Z]+/g);
-	if (isNaN(+time[0])) throw new TypeError(`\'${str}\' must be a valid time string`);
 
-	let parsed = 0;
+	// Check if the provided string is already a number
+	let _str2num = Number(str);
+	if (!isNaN(_str2num)) return _str2num;
 
-    switch (time[1]) {
-        case "y": parsed = (+time[0] * 12 * 4 * 7 * 24 * 60 * 60 * 1000); break;
-        case "mth": parsed = (+time[0] * 4 * 7 * 24 * 60 * 60 * 1000); break;
-        case "w": parsed = (+time[0] * 7 * 24 * 60 * 60 * 1000); break;
-        case "d": parsed = (+time[0] * 24 * 60 * 60 * 1000); break;
-        case "h": parsed = (+time[0] * 60 * 60 * 1000); break;
-        case "m": parsed = (+time[0] * 60 * 1000); break;
-        case "s": parsed = (+time[0] * 1000); break;
-        case "ms": parsed = (+time[0]); break;
-    }
+	// Match time formats found in the given string query
+	let timeQuery = str.matchAll(/([\d]+)([a-zA-Z]+)/g);
+	let isNegative = str.startsWith("-");
+	let sum = 0;
 
-    if (options.fromNow) parsed = isNegative
-        ? Date.now() - parsed
-        : Date.now() + parsed;
+	// Iterate through each match and preform the conversion operation on them
+	for (let query of timeQuery) {
+		let time = Number(query[1]);
+		let op = query[2] || null;
 
-    switch (options.type) {
-        case "s": return isNegative && !options.fromNow ? -_nT.msToSec(parsed) : _nT.msToSec(parsed);
-        case "ms": return isNegative && !options.fromNow ? -parsed : parsed;
-        default: return isNegative && !options.fromNow ? -parsed : parsed;
-    }
+		// Error checking
+		if (isNaN(time) || !op)
+			throw new TypeError(`\'${str}\' must be in a parsable time format. Example: '24h' or '1h 30m'`);
+
+		let _parsed = 0;
+
+		// prettier-ignore
+		switch (op) {
+			case "y": _parsed = time * 12 * 4 * 7 * 24 * 60 * 60 * 1000; break;
+			case "mth": _parsed = time * 4 * 7 * 24 * 60 * 60 * 1000; break;
+			case "w": _parsed = time * 7 * 24 * 60 * 60 * 1000; break;
+			case "d": _parsed = time * 24 * 60 * 60 * 1000; break;
+			case "h": _parsed = time * 60 * 60 * 1000; break;
+			case "m": _parsed = time * 60 * 1000; break;
+			case "s": _parsed = time * 1000; break;
+			case "ms": _parsed = time; break;
+		}
+
+		// Add to the sum
+		sum += _parsed;
+	}
+
+	/* - - - - - { Return the Result } - - - - - */
+	if (options.fromNow) isNegative ? (sum = Date.now() - sum) : (sum = Date.now() + sum);
+	if (options.type === "s") sum = _nT.msToSec(sum);
+	if (!options.fromNow && isNegative) sum = -sum;
+
+	return sum;
 }
 
 /** @typedef eta_options
